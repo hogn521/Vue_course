b8f24fce31e6398884d0e1aadb002df5f822eab3
John Test <john52102@126.com>
2026-07-02 07:30:51 +0800

    完成 todolist

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -2,30 +2,50 @@
   <div>
     <h1>根组件</h1>
     <todoList :todolist="todolist"></todoList>
-    <todoButton></todoButton>
+    <todoButton @changeActive="hangeFun"></todoButton>
   </div>
-
 </template>
 
 <script>
-import todoList from './components/todoList.vue';
-import todoButton from './components/todoButton.vue';
+import todoList from "./components/todoList.vue";
+import todoButton from "./components/todoButton.vue";
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
+    return {
+      todolist: [
+        { id: 1, task: "吃饭", isCompleted: true },
+        { id: 2, task: "睡觉", isCompleted: false },
+        { id: 3, task: "打游戏", isCompleted: true },
+        { id: 4, task: "学习", isCompleted: true },
+      ],
+      active: 0,
+    };
+  },
 
-    return{
-      todolist:[
-        {id:1,task:"吃饭",isCompleted:true},
-        {id:2,task:"睡觉",isCompleted:false},
-        {id:3,task:"打游戏",isCompleted:true},
-        {id:4,task:"学习",isCompleted:true},
-      ]
+  // 使用计算属性来实现当列表里的isCompleted改变的现实
+  computed:{
+    todolist(){
+      if(this.active === 0){
+        return this.todolist
+      }
+      else if(this.active === 1){
+        return this.todolist.filter(item => item.isCompleted)
+      }
+      else{
+        return this.todolist.filter(item => !item.isCompleted)
+      }
     }
+  },
+  methods: {
+    hangeFun(index) {
+      this.active = index;
+      console.log(this.active);
+    },
   },
   components: {
     todoList,
-    todoButton
-  }
-}
+    todoButton,
+  },
+};
 </script>
