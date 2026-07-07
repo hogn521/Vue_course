33d9ee2c6ecc408d7ee10ee938970e5fc4a9a882
John Test <john52102@126.com>
2026-07-07 20:04:23 +0800

    本地提交的一些文件

---
+++ Javascript/Anonymous_function/anoy.js
@@ -0,0 +1,26 @@
+// 1、函数表达式
+let fun = function(){
+    console.log("我是函数表达式");
+}
+
+// 2、回调函数
+
+
+// 3、立即执行函数
+
+
+// 变量和函数提升
+// console.log(myLet);
+// let myLet = "hello";
+
+
+// 箭头函数的this指向
+
+const obj = {
+    uname:"老师",
+    sayHi: function(){
+        
+        let i = 10
+
+    }
+}
